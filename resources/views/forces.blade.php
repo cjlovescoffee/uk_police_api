a46ec75d6f4814bb11da965c49e0b2e7b011ef08
cjlovescoffee <cjlovescoffee@gmail.com>
2018-11-19 18:23:56 +0000

    Add example forces api call

--- resources/views/forces.blade.php
+++ resources/views/forces.blade.php
@@ -1,3 +1,21 @@
 @extends('layouts.master')
+@php
+  require_once('../resources/api/police.php');
+  $POLICE = new Police();
+  $forces = $POLICE->forces();
+@endphp
 @section('main')
+  <section>
+    <div class="container">
+      <div class="row">
+        @foreach($forces as $force)
+          <pre>
+            @php
+              print_r($force);
+            @endphp
+          </pre>
+        @endforeach
+      </div>
+    </div>
+  </section>
 @endsection
